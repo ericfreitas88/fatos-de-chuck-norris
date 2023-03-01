6b0b81b851487cc041900f545e60dfe7fce42bbc
Eric Vilarino de Freitas <ericfreitas88@gmail.com>
2023-03-01 19:13:31 -0300

    Atualizando arquivos de estilo e add nova pasta com os aquivos de fontesque serão utilizadas na página

--- js/index.js
+++ js/index.js
@@ -1,20 +1,30 @@
 $(document).ready(function () {
     retornaFatosChuckNorris();
 
-    $(".novo_fato").on("click", function () {  
-        retornaFatosChuckNorris();
+    $(".novo_fato").on("click", function () {
+        var chuck_fato = $(".chuck-fato");
+        var btn = $(this);
+
+        chuck_fato.text("");
+        btn.html(`<img src="./assets/chuck-icon.gif" alt="Gif Chuck Dancing">`);
+        setTimeout (function () {
+            retornaFatosChuckNorris();
+        }, 1500)
     })
 
     function retornaFatosChuckNorris() {
         var chuck_fato = $(".chuck-fato");
+        var btn = $(".novo_fato");
+
         $.ajax({
             type: "GET",
             url: "https://api.chucknorris.io/jokes/random",
             dataType: "json",
             success: function (res) {
-                console.table(res);
                 chuck_fato.html(res.value);
                 chuck_fato.attr("cite", res.url)
+                btn.html("Pr&oacute;ximo fato");
+                console.log("oi")
             }
         });
     }
